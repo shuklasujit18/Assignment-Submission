99ae2e17317796103880c4619d2ed5f7c36db2ca
shuklasujit18 <81384349+shuklasujit18@users.noreply.github.com>
2021-03-26 00:31:13 +0530

    Lecture 4 Assignment Submitted

---
+++ Lecture 4 Assignment By Sujit/fetch.js
@@ -0,0 +1,9 @@
+var x = document.getElementById('demo');
+console.log(x);
+
+var ele = document.getElementsByTagName('article');
+console.log(ele);
+
+var ele = document.getElementsByClassName('demo');
+console.log(ele);
+(ele[0]).innerText="Class Content is change via javascript here";
